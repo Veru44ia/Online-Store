c1948fbf4aec45895d8ceed89ebb2e0015051979
rannue <yavianbo@gmail.com>
2022-12-20 18:25:50 +0300

    refactor: improve render logic

--- src/core/templates/components.ts
+++ src/core/templates/components.ts
@@ -1,12 +1,17 @@
 abstract class Component {
   protected container: HTMLElement;
+  protected position: InsertPosition;
+  protected content: string;
 
   constructor(tagName: string, className: string) {
     this.container = document.createElement(tagName);
     this.container.className = className;
+    this.position = 'afterbegin'
+    this.content = ``
   }
 
   render() {
+    this.container.insertAdjacentHTML(this.position, this.content)
     return this.container;
   }
 }
