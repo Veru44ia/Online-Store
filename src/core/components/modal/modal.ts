013bd2d65a8cfb915dfd7631cdb806756c42af76
Veru44ia <vera3188@gmail.com>
2022-12-28 21:15:39 +0300

    feat: add validation of inputs

--- src/core/components/modal/modal.ts
+++ src/core/components/modal/modal.ts
@@ -2,11 +2,13 @@ import "./modal.css";
 
 class Modal {
   container: HTMLElement;
+  inputs: string[];
   imgSrc: string;
 
   constructor(className: string) {
     this.container = document.createElement("div");
     this.container.className = className;
+    this.inputs = ["name", "email-input", "tel-input", "address-input", "card", "valid", "cvv"];
     this.imgSrc = "./assets/credit_card_icon.svg";
   }
 
@@ -28,6 +30,182 @@ class Modal {
         this.container.remove();
       }
     });
+  }
+
+  checkValidate() {
+    this.validateOnSubmit();
+    this.validateOnEntry();
+  }
+
+  validateOnSubmit() {
+    const form = this.container.querySelector(".form") as HTMLFormElement;
+    form.addEventListener("submit", (event: Event) => {
+      event.preventDefault();
+      let res: boolean[] = [];
+      this.inputs.forEach((item) => {
+        const input = this.container.querySelector(`.${item}`) as HTMLInputElement;
+        this.checkInputs(input);
+        res.push(this.checkInputs(input));
+      });
+      res = res.filter((item) => !item);
+      if (res.length === 0) {
+        const modalWrapper = this.container.querySelector(".modal-wrapper") as HTMLDivElement;
+        modalWrapper.innerHTML = "";
+        const paragraph = document.createElement("p");
+        paragraph.classList.add("order");
+        paragraph.textContent = `Thank you for your order! Redirect to the store after 5 sec!`;
+        modalWrapper.append(paragraph);
+        localStorage.removeItem("cart--products");
+      }
+    });
+  }
+
+  validateOnEntry() {
+    this.inputs.forEach((item) => {
+      const input = this.container.querySelector(`.${item}`) as HTMLInputElement;
+      input.addEventListener("input", () => {
+        this.checkInputs(input);
+      });
+    });
+  }
+
+  checkInputs(input: HTMLInputElement) {
+    const cardImg = this.container.querySelector(".card-image") as HTMLImageElement;
+    let isValid = true;
+
+    const error = (input: HTMLInputElement) => {
+      (input.previousElementSibling as HTMLSpanElement).classList.add("span-error");
+      (input.nextElementSibling as HTMLSpanElement).style.display = "inline-block";
+      input.classList.add("border-error");
+    };
+
+    const success = (input: HTMLInputElement) => {
+      (input.previousElementSibling as HTMLSpanElement).classList.remove("span-error");
+      (input.nextElementSibling as HTMLSpanElement).style.display = "none";
+      input.classList.remove("border-error");
+    };
+
+    const checkEmail = (email: HTMLInputElement) => {
+      const pattern =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (pattern.test(email.value)) {
+        success(email);
+        isValid = true;
+      } else {
+        error(email);
+        isValid = false;
+      }
+    };
+
+    const checkName = (name: HTMLInputElement) => {
+      const pattern = /([A-Z]{1}[a-z-]{2,}\s[A-Z]{1}[a-z-]{2,})+$/;
+
+      if (pattern.test(name.value)) {
+        success(name);
+        isValid = true;
+      } else {
+        error(name);
+        isValid = false;
+      }
+    };
+
+    const checkAddress = (address: HTMLInputElement) => {
+      const pattern = /(.{5,}\s.{5,}\s.{5,})+$/;
+
+      if (pattern.test(address.value)) {
+        success(address);
+        isValid = true;
+      } else {
+        error(address);
+        isValid = false;
+      }
+    };
+
+    const checkTel = (tel: HTMLInputElement) => {
+      const pattern = /^\+[0-9]{9,}$/;
+
+      if (pattern.test(tel.value)) {
+        success(tel);
+        isValid = true;
+      } else {
+        error(tel);
+        isValid = false;
+      }
+    };
+
+    const checkcardNumber = (cardNumber: HTMLInputElement) => {
+      const pattern = /^[0-9]{4}[0-9]{4}[0-9]{4}[0-9]{4}$/;
+      if (cardNumber.value[0] === "4") {
+        this.imgSrc = "./assets/Visa.svg";
+        cardImg.setAttribute("src", this.imgSrc);
+      } else if (cardNumber.value[0] === "5") {
+        this.imgSrc = "./assets/Mastercard.svg";
+        cardImg.setAttribute("src", this.imgSrc);
+      } else if (cardNumber.value[0] === "6") {
+        this.imgSrc = "./assets/Maestro.svg";
+        cardImg.setAttribute("src", this.imgSrc);
+      } else {
+        this.imgSrc = "./assets/credit_card_icon.svg";
+        cardImg.setAttribute("src", this.imgSrc);
+      }
+
+      if (pattern.test(cardNumber.value) && cardNumber.value.length === cardNumber.maxLength) {
+        success(cardNumber);
+        isValid = true;
+      } else if (cardNumber.value.length > cardNumber.maxLength) {
+        cardNumber.value = cardNumber.value.slice(0, cardNumber.maxLength);
+      } else {
+        error(cardNumber);
+        isValid = false;
+      }
+    };
+
+    const checkCvv = (cvv: HTMLInputElement) => {
+      const pattern = /^[0-9]{1,3}$/;
+
+      if (pattern.test(cvv.value) && cvv.value.length === cvv.maxLength) {
+        success(cvv);
+        isValid = true;
+      } else if (cvv.value.length > cvv.maxLength) {
+        cvv.value = cvv.value.slice(0, cvv.maxLength);
+      } else {
+        error(cvv);
+        isValid = false;
+      }
+    };
+
+    const checkValid = (valid: HTMLInputElement) => {
+      const pattern = /^(0?[1-9]|1[0-2])[0-9]{2}$/;
+
+      if (pattern.test(valid.value) && valid.value.length === valid.maxLength) {
+        success(valid);
+        isValid = true;
+      } else if (valid.value.length > valid.maxLength) {
+        valid.value = valid.value.slice(0, valid.maxLength);
+      } else {
+        error(valid);
+        isValid = false;
+      }
+    };
+
+    if (input.getAttribute("name") === "e-mail") {
+      checkEmail(input);
+    } else if (input.getAttribute("name") === "name") {
+      checkName(input);
+    } else if (input.getAttribute("name") === "address") {
+      checkAddress(input);
+    } else if (input.getAttribute("name") === "phone") {
+      checkTel(input);
+    } else if (input.getAttribute("name") === "card-number") {
+      checkcardNumber(input);
+    } else if (input.getAttribute("name") === "СVV") {
+      checkCvv(input);
+    } else if (input.getAttribute("name") === "valid") {
+      checkValid(input);
+    }
+
+    return isValid;
   }
 
   render() {
@@ -96,6 +274,8 @@ class Modal {
       </div>  
     `;
 
+    this.closeModal();
+    this.checkValidate();
     return this.container;
   }
 }
