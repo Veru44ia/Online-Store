7dbf45f3c58b4b386ca425e55236a63c1621004c
Veru44ia <vera3188@gmail.com>
2023-01-04 23:37:45 +0300

    feat: add increase quantity of particular product and its delete

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -5,19 +5,27 @@ import { IProduct } from "../../types/types";
 import Modal from "../../core/components/modal/modal";
 
 // mock data (delete later)
-localStorage.setItem("cart--products", JSON.stringify(products.slice(0, 6)));
+localStorage.setItem("productInCart", JSON.stringify(products.slice(0, 6)));
 
 class CartPage extends Page {
   private modal: Modal;
+  totalPrice: number;
+  totalProducts: number;
 
   constructor() {
     super();
+    this.totalPrice = 0;
+    this.totalProducts = 0;
     this.modal = new Modal("modal-container");
   }
 
   getFromLocalStorage() {
-    if (localStorage.getItem("cart--products")) {
-      this.renderCartPage(JSON.parse(localStorage.getItem("cart--products") || "[]"));
+    if (localStorage.getItem("productInCart")) {
+      const products: IProduct[] = JSON.parse(localStorage.getItem("productInCart") || "[]");
+      products.map(function (item, index) {
+        item.orderNumber = index + 1;
+      });
+      this.renderProductsList(products);
     } else {
       this.container.innerHTML = "";
       const paragraph = document.createElement("p");
@@ -27,42 +35,59 @@ class CartPage extends Page {
     }
   }
 
-  renderCartPage(products: IProduct[]) {
-    const cartContainerWrapper = document.createElement("div");
-    cartContainerWrapper.classList.add("cart-container-wrapper");
-    (cartContainerWrapper as HTMLElement).innerHTML = "";
-
+  renderPagination() {
     const pagination = document.createElement("div");
     pagination.classList.add("pagination-container");
 
     pagination.innerHTML = `
       <span class="pagination-title">Items on page: </span>
-      <input type="number" min="1" value="1" class="pagination-items-on-page">
+      <input type="number" min="1" value="3" class="pagination-items-on-page">
       <span class="pagination-page">Page:</span>
       <input type="number" min="1" value="1" readonly class="pagination-number">
       <button type="button" class="pagination-prev"><</button>
       <button type="button" class="pagination-next">></button>
     `;
 
+    this.container.prepend(pagination);
+  }
+
+  renderCartContainer() {
+    const cartContainerWrapper = document.createElement("div");
+    cartContainerWrapper.classList.add("cart-container-wrapper");
+    this.container.append(cartContainerWrapper);
+  }
+
+  renderProductsList(products: IProduct[]) {
+    const cartContainerWrapper = this.container.querySelector(
+      ".cart-container-wrapper"
+    ) as HTMLDivElement;
+    (cartContainerWrapper as HTMLElement).innerHTML = "";
+
     const cartContainer = document.createElement("div");
     cartContainer.classList.add("cart-container");
 
     products.forEach(
-      ({
-        title,
-        description,
-        price,
-        discountPercentage,
-        rating,
-        stock,
-        brand,
-        category,
-        thumbnail,
-      }) => {
+      (
+        {
+          title,
+          description,
+          price,
+          discountPercentage,
+          rating,
+          stock,
+          brand,
+          category,
+          thumbnail,
+          orderNumber,
+        },
+        index
+      ) => {
         const cardsBlock = document.createElement("div");
         cardsBlock.classList.add("cart-product-item");
-
+        this.totalProducts = products.length;
+        this.totalPrice += price;
         cardsBlock.innerHTML = `
+          <div class="order-number">${orderNumber}</div>
           <img src="${thumbnail}" alt="${title}" class="card-img">
           <div class="card-profile">
             <div class="card-top">
@@ -77,26 +102,67 @@ class CartPage extends Page {
               </div>
               <span class="card-stock">Stock: ${stock}</span>
               <div class="controls-wrapper">
-                <button type="button" onclick="this.nextElementSibling.stepDown()" class="button-minus">-</button>
-                <input type="number" min="1" value="1" readonly class="number">
-                <button type="button" onclick="this.previousElementSibling.stepUp()" class="button-plus">+</button>
+                <button type="button" onclick="this.nextElementSibling.stepDown()" class="button button-minus">-</button>
+                <input type="number" min="0" max="${stock}" value="1" readonly class="number">
+                <button type="button" onclick="this.previousElementSibling.stepUp()" class="button button-plus">+</button>
               </div>
-              <span class="card-price">Price: ${price} USD</span>
+              <span class="card-price">Price: <span class="card-price-num">${price}</span> USD</span>
             </div> 
           </div>
         `;
 
         (cartContainer as HTMLElement).append(cardsBlock);
+
+        cardsBlock.addEventListener("click", () => {
+          const number = cardsBlock.querySelector(".number") as HTMLInputElement;
+          const numberValue = (cardsBlock.querySelector(".number") as HTMLInputElement).value;
+          number.setAttribute("value", `${numberValue}`);
+          const cardPriceNum = cardsBlock.querySelector(".card-price-num") as HTMLSpanElement;
+          if (+numberValue < 1) {
+            const productInCart = JSON.parse(localStorage.getItem("productInCart") || "[]");
+            productInCart.splice(index, 1);
+            localStorage.setItem("productInCart", JSON.stringify(productInCart));
+            this.getFromLocalStorage();
+          }
+          cardPriceNum.innerHTML = (+numberValue * products[index].price).toString();
+          this.calculateTotalProducts();
+          this.calculateTotalPrice();
+        });
       }
     );
 
+    cartContainerWrapper.append(cartContainer);
+  }
+
+  calculateTotalProducts() {
+    const numbers = Array.from(this.container.querySelectorAll(".number")).map((item) => {
+      return +(item as HTMLInputElement).value;
+    });
+    const totalProductsValue = this.container.querySelector(".products-bold") as HTMLElement;
+    this.totalProducts = numbers.reduce((a, b) => a + b, 0);
+    totalProductsValue.innerHTML = `${this.totalProducts}`;
+    return this.totalProducts;
+  }
+
+  calculateTotalPrice() {
+    const cardPriceNums = this.container.querySelectorAll(".card-price-num");
+    const totalPriceValue = this.container.querySelector(".price-bold") as HTMLElement;
+    const totalPriceArr = Array.from(cardPriceNums).map((item) => {
+      return Number(item.innerHTML);
+    });
+
+    this.totalPrice = totalPriceArr.reduce((a, b) => a + b, 0);
+    totalPriceValue.innerHTML = `${this.totalPrice} USD`;
+  }
+
+  renderTotal() {
     const total = document.createElement("div");
     total.classList.add("total-container");
 
     total.innerHTML = `
       <div class="total-wrapper">
-        <p class="total-products">Products: <b>5</b></p>
-        <p class="total-price">Total: <b>290 USD</b></p>
+        <p class="total-products">Products: <b class="products-bold">${this.totalProducts}</b></p>
+        <p class="total-price">Total: <b class="price-bold">${this.totalPrice} USD</b></p>
       </div>
       <div class="total-wrap">
         <input type="search" class="total-search" placeholder="Enter promo code">
@@ -105,14 +171,14 @@ class CartPage extends Page {
       <button type="button" class="buy-btn">Buy now</button>
     `;
 
-    cartContainerWrapper.append(pagination);
-    cartContainerWrapper.append(cartContainer);
-    cartContainerWrapper.append(total);
-    this.container.append(cartContainerWrapper);
+    this.container.append(total);
   }
 
   render() {
+    this.renderPagination();
+    this.renderCartContainer();
     this.getFromLocalStorage();
+    this.renderTotal();
     this.modal.openModal();
     return this.container;
   }
