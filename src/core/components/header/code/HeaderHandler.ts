a215366b0cdfad391eb8e65ba02ebcd434331195
rannue <yavianbo@gmail.com>
2023-01-07 22:08:23 +0300

    feat: add HeaderHandler

---
+++ src/core/components/header/code/HeaderHandler.ts
@@ -0,0 +1,23 @@
+import { IProduct } from "../../../data";
+
+export class HeaderHandler {
+
+  static setPrice() {
+    const totalPrice = document.getElementById('cart-total') as HTMLDivElement;
+    let storageProducts: IProduct[] = JSON.parse(localStorage.getItem("productInCart") || "[]");
+    let priceText = 0
+    storageProducts.forEach((item) => {
+      priceText += item.price
+    })
+    totalPrice.innerText = `${priceText.toString()} USD`
+  }
+
+  static setCount() {
+    const totalCount = document.getElementById('cart-count') as HTMLDivElement;
+    let storageProducts: IProduct[] = JSON.parse(localStorage.getItem("productInCart") || "[]");
+    console.log(totalCount)
+    storageProducts.length > 0
+      ? totalCount.innerHTML = `<h6>${storageProducts.length}</h6>`
+      : totalCount.innerHTML = `<h6>0</h6>`
+  }
+}
